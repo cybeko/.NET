22ca243b0704683014f625808e73e962a6de1a12
jarviscsp@gmail.com <jarviscsp@gmail.com>
2024-10-13 23:22:02 +0300

    initial

---
+++ hw04/hw04/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw04
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            Random rand = new Random();
+
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(0, 6);
+            }
+
+            Console.WriteLine("Original array: " + string.Join(", ", arr));
+
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    arr[index++] = arr[i];
+                }
+            }
+
+            for (int i = index; i < arr.Length; i++)
+            {
+                arr[i] = -1;
+            }
+
+            Console.WriteLine("Final array: " + string.Join(", ", arr));
+        }
+        static void Task2()
+        {
+            Console.Write("Enter size of the matrix: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[N, N];
+
+            int x = N / 2;
+            int y = N / 2;
+
+            matrix[x, y] = 1;
+            int value = 2;
+
+            int[,] directions = { 
+                { 0, -1 }, 
+                { -1, 0 }, 
+                { 0, 1 }, 
+                { 1, 0 } 
+            };
+
+            int steps = 1;
+            int curDir = 0;
+
+            while (value <= N * N)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < steps; j++)
+                    {
+                        x += directions[curDir, 0];
+                        y += directions[curDir, 1];
+
+                        if (x >= 0 && x < N && y >= 0 && y < N)
+                        {
+                            matrix[x, y] = value++;
+                        }
+                    }
+                    curDir = (curDir + 1) % 4;
+                }
+                steps++;
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (matrix[i, j] < 10)
+                    {
+                        Console.Write(" " + matrix[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Task3()
+        {
+            Console.Write("Enter number of rows: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number of columns: ");
+            int M = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[N, M];
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    matrix[i, j] = random.Next(0, 101);
+                }
+            }
+
+            Console.WriteLine("Original matrix:");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write(matrix[i, j].ToString().PadLeft(4));
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter number of columns to shift: ");
+            int shift = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter shift direction (r or l): ");
+            string dir = Console.ReadLine().ToLower();
+
+            shift = shift % M;
+
+            for (int i = 0; i < N; i++)
+            {
+                int[] row = new int[M];
+
+                if (dir == "r")
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        row[(j + shift) % M] = matrix[i, j];
+                    }
+                }
+                else if (dir == "l")
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        row[j] = matrix[i, (j + shift) % M];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid direction");
+                    return;
+                }
+
+                for (int j = 0; j < M; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+
+            Console.WriteLine("Shifted matrix:");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write(matrix[i, j].ToString().PadLeft(4));
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+        }
+    }
+}
---
+++ hw05/hw05/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw05
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Enter shift (0-25): ");
+            int shift;
+            while (!int.TryParse(Console.ReadLine(), out shift) || shift < 0 || shift > 25)
+            {
+                Console.Write("Incorrect input. ");
+            }
+
+            StringBuilder result = new StringBuilder();
+            Console.WriteLine("1 - cipher, 2 - decipher: ");
+            int action;
+            while (!int.TryParse(Console.ReadLine(), out action) || (action != 1 && action != 2))
+            {
+                Console.Write("Incorrect input.");
+            }
+
+            if (action == 2)
+            {
+                shift = 26 - (shift % 26);
+            }
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                    char transformedChar = (char)((((c - baseChar) + shift) % 26) + baseChar);
+                    result.Append(transformedChar);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            if (action == 1)
+            {
+                Console.WriteLine($"Ciphered: {result.ToString()}");
+            }
+            else
+            {
+                Console.WriteLine($"Deciphered: {result.ToString()}");
+            }
+
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Enter a math equation:");
+            string input = Console.ReadLine();
+
+            input = input.Replace(" ", "");
+
+            double result = 0;
+            double currentNumber = 0;
+            char operation = '+';
+
+            StringBuilder expression = new StringBuilder(input);
+            string[] tokens = expression.ToString().Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var token in tokens)
+            {
+                if (double.TryParse(token, out currentNumber))
+                {
+                    switch (operation)
+                    {
+                        case '+':
+                            result += currentNumber;
+                            break;
+                        case '-':
+                            result -= currentNumber;
+                            break;
+                    }
+                }
+                else {
+                    throw new FormatException($"Wrong symbol: '{token}'");
+                }
+
+                if (input.Length > 0)
+                {
+                    if (input[0] == '+' || input[0] == '-')
+                    {
+                        operation = input[0];
+                        input = input.Substring(1);
+                    }
+                }
+            }
+            Console.WriteLine($"Result: {result}");
+            
+        }
+        static void Task3( )
+        {
+            Console.WriteLine("Enter text:");
+            string inp = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+            bool capNext = true;
+            for (int i = 0; i < inp.Length; i++)
+            {
+                char curChar = inp[i];
+
+                if (curChar == '.' || curChar == '!' || curChar == '?')
+                {
+                    result.Append(curChar);
+                    capNext = true;
+                }
+                else if (char.IsWhiteSpace(curChar))
+                {
+                    result.Append(curChar);
+                }
+                else
+                {
+                    if (capNext && char.IsLetter(curChar))
+                    {
+                        result.Append(char.ToUpper(curChar));
+                        capNext = false;
+                    }
+                    else
+                    {
+                        result.Append(curChar);
+                    }
+                }
+            }
+            Console.WriteLine("Result:");
+            Console.WriteLine(result.ToString());
+        }
+        static void Task4( ) 
+        {
+
+            string inputText, forbiddenWord;
+            int replCount = 0;
+
+            Console.WriteLine("Enter text:");
+            inputText = Console.ReadLine();
+
+            Console.WriteLine("Enter forbidden word:");
+            forbiddenWord = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+            string replacedWord = new string('*', forbiddenWord.Length); 
+
+            string[] words = inputText.Split(new[] { ' ', '\n', '\r', '.', ',', '!', '?', ';', ':', '\'', '\"' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                if (word.ToLower() == forbiddenWord.ToLower())
+                {
+                    result.Append(replacedWord);
+                    replCount++;
+                }
+                else
+                {
+                    result.Append(word);
+                }
+                result.Append(" ");
+            }
+            Console.WriteLine("Result:");
+            Console.WriteLine(result.ToString().TrimEnd());
+            Console.WriteLine($"{replCount} '{forbiddenWord}' replaced.");
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+
+
+        }
+    }
+}
