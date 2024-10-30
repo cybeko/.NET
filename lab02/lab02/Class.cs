a22e813ce94b2a99126ac43ecb6304e3ae434b5a
jarviscsp@gmail.com <jarviscsp@gmail.com>
2024-10-30 20:25:45 +0200

    initial

---
+++ lab02/lab02/Class.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02
+{
+    struct ComplexNumber
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+        public ComplexNumber() : this(0, 0) { }
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+        public static ComplexNumber Add(ComplexNumber num1, ComplexNumber num2)
+        {
+            return new ComplexNumber(num1.Real + num2.Real, num1.Imaginary + num2.Imaginary);
+        }
+        public static ComplexNumber Subtract(ComplexNumber num1, ComplexNumber num2)
+        {
+            return new ComplexNumber(num1.Real - num2.Real, num1.Imaginary - num2.Imaginary);
+        }
+        public static ComplexNumber Divide(ComplexNumber num1, ComplexNumber num2)
+        {
+            double denom = num2.Real * num2.Real + num2.Imaginary * num2.Imaginary;
+            if (denom == 0)
+                throw new DivideByZeroException("Cannot divide by 0.");
+
+            double real = (num1.Real * num2.Real + num1.Imaginary * num2.Imaginary) / denom;
+            double imaginary = (num1.Imaginary * num2.Real - num1.Real * num2.Imaginary) / denom;
+            return new ComplexNumber(real, imaginary);
+        }
+        public static ComplexNumber Multiply(ComplexNumber num1, ComplexNumber num2)
+        {
+            double realPart = num1.Real * num2.Real - num1.Imaginary * num2.Imaginary;
+            double imaginaryPart = num1.Real * num2.Imaginary + num1.Imaginary * num2.Real;
+            return new ComplexNumber(realPart, imaginaryPart);
+        }
+        public override readonly string ToString() => $"{Real} + {Imaginary}i";
+    }
+}
---
+++ lab02/lab02/Program.cs
@@ -0,0 +1,12 @@
+﻿using lab02;
+
+ComplexNumber complex1 = new(3, 4);
+ComplexNumber complex2 = new(2, 2);
+
+Console.WriteLine("Complex number 1: " + complex1);
+Console.WriteLine("Complex number 2: " + complex2);
+
+Console.WriteLine(ComplexNumber.Add(complex1, complex2));
+Console.WriteLine(ComplexNumber.Subtract(complex1, complex2));
+Console.WriteLine(ComplexNumber.Multiply(complex1, complex2));
+Console.WriteLine(ComplexNumber.Divide(complex1, complex2));
