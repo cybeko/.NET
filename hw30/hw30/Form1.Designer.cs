dfe4d9dffc24ddfef576073e50ebf111cf0c1dc9
jarviscsp@gmail.com <jarviscsp@gmail.com>
2025-02-04 19:48:02 +0200

    initial

---
+++ hw30/hw30/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace hw30
+{
+    partial class ProcessManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listBoxProc = new System.Windows.Forms.ListBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxProcName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(223, 23);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 22);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listBoxProc
+            // 
+            this.listBoxProc.FormattingEnabled = true;
+            this.listBoxProc.Location = new System.Drawing.Point(22, 62);
+            this.listBoxProc.Name = "listBoxProc";
+            this.listBoxProc.Size = new System.Drawing.Size(184, 225);
+            this.listBoxProc.TabIndex = 2;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(223, 265);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 22);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxProcName
+            // 
+            this.textBoxProcName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxProcName.Location = new System.Drawing.Point(22, 23);
+            this.textBoxProcName.Name = "textBoxProcName";
+            this.textBoxProcName.Size = new System.Drawing.Size(184, 22);
+            this.textBoxProcName.TabIndex = 4;
+            // 
+            // ProcessManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 321);
+            this.Controls.Add(this.textBoxProcName);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.listBoxProc);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "ProcessManager";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listBoxProc;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBoxProcName;
+    }
+}
+
---
+++ hw30/hw30/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace hw30
+{
+    public partial class ProcessManager : Form
+    {
+        public ProcessManager()
+        {
+            InitializeComponent();
+            LoadProcessList();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string proc = textBoxProcName.Text.Trim();
+            if (!string.IsNullOrEmpty(proc))
+            {
+
+                    Process myProcess = new Process();
+                    myProcess.StartInfo = new ProcessStartInfo(proc);
+                    myProcess.Start();
+                    MessageBox.Show($"Started process: {proc}");
+                    LoadProcessList();
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (listBoxProc.SelectedItem != null)
+            {
+                string selectedProcessName = listBoxProc.SelectedItem.ToString();
+
+                Process[] processes = Process.GetProcessesByName(selectedProcessName);
+
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                }
+
+                MessageBox.Show($"Killed process: {selectedProcessName}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadProcessList();
+            }
+        }
+        private void LoadProcessList()
+        {
+            listBoxProc.Items.Clear();
+            var processNames = Process.GetProcesses().Select(p => p.ProcessName).Distinct().OrderBy(name => name);
+
+            foreach (var proc in processNames)
+            {
+                listBoxProc.Items.Add(proc);
+            }
+        }
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            LoadProcessList();
+        }
+    }
+}
