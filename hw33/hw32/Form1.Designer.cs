3a6a59eea61087c3cadd5fb150a77c7076343d99
jarviscsp@gmail.com <jarviscsp@gmail.com>
2025-02-07 18:53:35 +0200

    initial

---
+++ hw33/hw32/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace hw32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonFrom = new System.Windows.Forms.Button();
+            this.buttonTo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Location = new System.Drawing.Point(99, 32);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(228, 20);
+            this.textBoxFrom.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "From Where";
+            // 
+            // buttonFrom
+            // 
+            this.buttonFrom.Location = new System.Drawing.Point(342, 30);
+            this.buttonFrom.Name = "buttonFrom";
+            this.buttonFrom.Size = new System.Drawing.Size(75, 23);
+            this.buttonFrom.TabIndex = 3;
+            this.buttonFrom.Text = "File...";
+            this.buttonFrom.UseVisualStyleBackColor = true;
+            this.buttonFrom.Click += new System.EventHandler(this.buttonFrom_Click);
+            // 
+            // buttonTo
+            // 
+            this.buttonTo.Location = new System.Drawing.Point(342, 69);
+            this.buttonTo.Name = "buttonTo";
+            this.buttonTo.Size = new System.Drawing.Size(75, 23);
+            this.buttonTo.TabIndex = 6;
+            this.buttonTo.Text = "File...";
+            this.buttonTo.UseVisualStyleBackColor = true;
+            this.buttonTo.Click += new System.EventHandler(this.buttonTo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To Where";
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(99, 71);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(228, 20);
+            this.textBoxTo.TabIndex = 4;
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(342, 109);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 23);
+            this.buttonCopy.TabIndex = 7;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(31, 109);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(296, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 163);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.buttonTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.buttonFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonFrom;
+        private System.Windows.Forms.Button buttonTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ hw33/hw32/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFrom_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fromFileDialog = new OpenFileDialog();
+            if (fromFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFrom.Text = fromFileDialog.FileName;
+            }
+        }
+
+        private void buttonTo_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog toFileDialog = new SaveFileDialog();
+            if (toFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxTo.Text = toFileDialog.FileName;
+            }
+        }
+
+        private async void buttonCopy_Click(object sender, EventArgs e)
+        {
+            string sourcePath = textBoxFrom.Text;
+            string destPath = textBoxTo.Text;
+
+            await CopyFileMethodAsync(sourcePath, destPath);
+            MessageBox.Show("Copied successfully");
+        }
+        private async Task CopyFileMethodAsync(string sourcePath, string destinationPath)
+        {
+            const int bufSize = 4096;
+
+            using (FileStream sourceStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read, FileShare.Read, bufSize, true))
+            using (FileStream destStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write, FileShare.None, bufSize, true))
+            {
+                byte[] buffer = new byte[bufSize];
+                long totalBytes = sourceStream.Length;
+                long copiedBytes = 0;
+                int bytesRead;
+
+                progressBar1.Invoke((Action)(() =>
+                {
+                    progressBar1.Maximum = 100;
+                    progressBar1.Value = 0;
+                }));
+
+                while ((bytesRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    await destStream.WriteAsync(buffer, 0, bytesRead);
+                    copiedBytes += bytesRead;
+
+                    int progress = (int)((copiedBytes * 100) / totalBytes);
+
+                    progressBar1.Invoke((Action)(() => progressBar1.Value = progress));
+                }
+            }
+        }
+    }
+}
